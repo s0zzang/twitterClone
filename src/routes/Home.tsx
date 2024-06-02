c93faddb526b93b8024b348e4c8c258e4cf60bc2
s0zzang <dlth8888@gmail.com>
2024-06-02 23:18:20 +0900

    feat. 로그아웃

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,10 @@
+import { auth } from "../firebase";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  const logOut = () => auth.signOut();
+  return (
+    <h1>
+      Home <button onClick={logOut}>LogOut</button>
+    </h1>
+  );
 }
